89c2fc1b54941d8826d0a5e2507d153de4e84361
Kent Davison <kdavison24@gmail.com>
2016-11-09 16:18:43 -0600

    First commit of dotfiles.

---
+++ .vim/bash-support/templates/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  test.sh
+# 
+#         USAGE:  ./test.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: YOUR NAME (), 
+#       COMPANY: 
+#       CREATED: 11/09/2016 03:52:00 PM CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
---
+++ .vimrc
@@ -0,0 +1,15 @@
+syntax on
+filetype plugin on
+set tabstop=3
+set shiftwidth=3
+set expandtab
+set bg=dark
+set hlsearch!
+set pastetoggle=<F2>
+
+highlight comment ctermfg=blue
+nnoremap <F3> :set hlsearch!<CR>
+
+let g:BASH_AuthorName   = 'Kent Davison'
+let g:BASH_Email        = ''
+let g:BASH_Company      = '' 
